a6f1f0b9c8fc42fdde1efb7507d3b0f685bd463f
Karim Mohammed <karim228223@gmail.com>
2024-06-27 19:57:06 +0300

    Add files via upload

---
+++ wuzzuf_scraping.py
@@ -0,0 +1,45 @@
+import requests
+import csv
+from bs4 import BeautifulSoup
+from itertools import zip_longest
+
+## the target link
+url = 'https://wuzzuf.net/search/jobs/?q=data%20analyst&a=hpb'
+job_title = []
+companies_name = []
+companies_location = []
+skills = []
+links = []
+salary = []
+
+## extracting the html code
+page = requests.get(url)
+content = page.content
+soup = BeautifulSoup(content, 'lxml')  # clarify the html code to be readable
+## extracting all jobs links from the page
+jobs = soup.find_all('h2', {'class': 'css-m604qf'})
+company_name = soup.find_all('a', {'class': 'css-17s97q8'})
+company_location = soup.find_all('span', {'class': 'css-5wys0k'})
+job_skills = soup.find_all('div', {'class': 'css-y4udm8'})
+
+## extracting the text from the lists above
+for i in range(len(company_location)):
+    job_title.append(jobs[i].text.strip())
+    companies_name.append(company_name[i].text.strip())
+    companies_location.append(company_location[i].text.strip())
+    skills.append(job_skills[i].text.strip())
+    links.append(jobs[i].find("a").attrs['href'])
+
+## extract the data from every job
+for link in links:
+    job_page = requests.get(link)
+    page_content = job_page.content
+    page_soup = BeautifulSoup(page_content,'html.parser')
+    salary.append(page_soup.find('span', {'class':'css-4xky9y'}))
+## creating csv file
+lists = [job_title, companies_name, companies_location, skills , salary]
+data = zip_longest(*lists)
+with open(r'D:\Karim\bootcamp\wuzzuf_jobs.csv', 'w') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Job Title', 'Company Name', 'Job location', 'skills required', 'Salary'])
+    writer.writerows(data)
